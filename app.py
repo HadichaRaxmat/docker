0680ca61e74bdc7ee6ddbf2412e0a7f566fe16f9
HadichaRaxmat <khadicha.raxmat@gmail.com>
2025-01-18 23:17:31 +0500

    Updated

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ print('hello world')
 
 import http.server
 
-
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -13,10 +12,12 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
         self.wfile.write(b'Hello world')
 
-
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    server_address = ('0.0.0.0', 8000)  # Слушаем на всех интерфейсах
     httpd = http.server.HTTPServer(server_address, SimpleHTTPRequestHandler)
 
     print("Server started on http://localhost:8000")
     httpd.serve_forever()
+
+#run
+#py app.py
